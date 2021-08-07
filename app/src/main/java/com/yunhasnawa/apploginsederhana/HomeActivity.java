5e2125a89bc0fb08ea5e58a49cbe2c3e9e8360cb
Yoga Meleniawan Pamungkas <64576201+yogameleniawan@users.noreply.github.com>
2021-08-07 16:38:36 +0700

    add list view

--- app/src/main/java/com/yunhasnawa/apploginsederhana/HomeActivity.java
+++ app/src/main/java/com/yunhasnawa/apploginsederhana/HomeActivity.java
@@ -8,16 +8,26 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Date;
 import java.util.Locale;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity  implements AdapterView.OnItemClickListener{
     private TextView txt_username;
     private TextView txt_hari;
+    private ArrayList<String> data;
+    private ListView listView;
+    private String[] listData = {"Mengerjakan tugas mobile", "Mengerjakan tugas web", "Mengerjakan tugas desain", "Mengerjakan tugas phyton"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +36,20 @@ public class HomeActivity extends AppCompatActivity {
         String currentDate = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
         this.txt_username = this.findViewById(R.id.txtv_user);
         this.txt_hari = this.findViewById(R.id.txtv_hari);
+        this.listView = this.findViewById(R.id.list_item);
+        data = new ArrayList<>();
+        getData();
+        ArrayAdapter adapter = new ArrayAdapter<>(this, R.layout.list_view, data);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
         String username = getIntent().getStringExtra("username");
 
         this.txt_username.setText("Selamat datang, "+username+" !");
         this.txt_hari.setText("Hari ini tanggal "+currentDate);
+    }
+
+    private void getData() {
+        Collections.addAll(data, listData);
     }
 
     public void logout(View view)
@@ -55,5 +75,11 @@ public class HomeActivity extends AppCompatActivity {
             this.finish();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onItemClick(AdapterView adapterView, View view, int position, long l) {
+        String getData = data.get(position);
+        Toast.makeText(this,getData,Toast.LENGTH_SHORT).show();
     }
 }
